a3086f4fd6e25267f9d50802369f1960d7910767
Maksim Tarkin <m.tarkin@monoagency.ru>
2025-03-07 16:21:05 +0300

    uncommit parse funcs

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"fmt"
 	scp_parser "scp-parser/src"
 )
 
 func main() {
 	scp_parser.CreateTable()
 	// data := []string{"/scp-1122", "/scp-001", "/scp-002"}
-	// arraySCP := scp_parser.ParseGetListSCP()[0:2]
-	// for _, item := range arraySCP {
-	// 	unit := scp_parser.ParseGetCurrentSCP(item)
-	// 	fmt.Println(unit)
-	// }
+	arraySCP := scp_parser.ParseGetListSCP()[0:2]
+	for _, item := range arraySCP {
+		unit := scp_parser.ParseGetCurrentSCP(item)
+		fmt.Println(unit)
+	}
 	// res := scp_parser.ParseGetCurrentSCP(data[0])
 	// fmt.Println(res)
 	// fmt.Println(res)
